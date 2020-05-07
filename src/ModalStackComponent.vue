617ea8c628f34e739a2fc404a44d0d7c8f5226e3
Simon Backx <hi@simonbackx.com>
2020-05-07 13:32:32 +0200

    Added missing components

---
+++ src/ModalStackComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <!-- Components taking up the whole document. Listens to show-modal -->
+    <div>
+        <NavigationController ref="navigationController" animation-type="modal" :root="root" @present="present" />
+        <StackComponent ref="stackComponent" @present="present" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Ref,Vue } from "vue-property-decorator";
+
+import { ComponentWithProperties } from "./ComponentWithProperties";
+import NavigationController from "./NavigationController.vue";
+import Popup from "./Popup.vue";
+import StackComponent from "./StackComponent.vue";
+
+@Component({
+    components: {
+        NavigationController,
+        StackComponent,
+    },
+})
+export default class ModalStackComponent extends Vue {
+    @Prop()
+    readonly root!: ComponentWithProperties;
+
+    @Ref()
+    stackComponent!: StackComponent;
+
+    present(component: ComponentWithProperties) {
+        if (component.modalDisplayStyle == "popup" && (this.$el as HTMLElement).offsetWidth > 800) {
+            this.stackComponent.show(new ComponentWithProperties(Popup, { root: component }));
+            return;
+        }
+
+        if (component.modalDisplayStyle == "overlay") {
+            this.stackComponent.show(component);
+            return;
+        }
+        (this.$refs.navigationController as NavigationController).push(component);
+    }
+}
+</script>
